68a115356ad6bb618fb2bd9833340a16cf5403aa
Aaron Vuong <avuong@esperanto.ai>
2021-07-16 10:37:49 -0700

    New Python File

---
+++ Scripts/Simple.py
@@ -0,0 +1,17 @@
+class Simple:
+
+    def __init__(self, variable_par):
+        self.integer = 20
+        self.dictionary = {"green eggs", "ham"}
+        self.var = variable_par
+
+    def Mult(self):
+        multiplier = self.variable_par * self.integer
+        return multiplier
+
+simple_object = Simple(20)
+simple_object.Mult()
+
+
+
+
