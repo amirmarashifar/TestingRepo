d88f5e57e5e970d38a315eea48937c1d29df6f17
amirmarashifar <86680606+amirmarashifar@users.noreply.github.com>
2021-07-19 10:12:30 -0700

    Update f1peeps.py

--- f1Driver/f1peeps.py
+++ f1Driver/f1peeps.py
@@ -1,12 +1,25 @@
 class driver: 
     x = "Danny ric is the goat"
     y = "Lewis Hamilton is not the goat"
+    tv = "Max Verstappen will hopefully win this year"
+    LHam = "Lewis Hamilton"
+    DRic = "Daniel Riccardo"
+    GrandPrixWinner = true
+
+    
     print(x)
     print(y)
     
 #this simeple yet elegant program will show you the best driver in formula 1 history
     def points(x):
         return numPoints
+    
+    def willWin(): 
+        numpoints = 0
+        if numpoints == 256: 
+            return GrandPrixWinner
+        else: 
+            return LHam
     
     
 
